34efcad4f8b0db35494b3f2a09f68fdfa2efd324
AbdullahSabry14 <sbryb404@gmail.com>
2026-03-13 22:01:26 +0200

    Update managers.py

--- managers.py
+++ managers.py
@@ -163,7 +163,8 @@ if not URL :
     # القفل البرمجي: "token_fetched" يمنع الكود من محاولة استخدام الـ code مجدداً بعد نجاح العملية
     if code and "token_fetched" not in st.session_state:
         try:
-            flow = st.session_state.flow
+            flow = Flow.from_client_config(json.loads(st.secrets["G_CRED"]), scopes,redirect_uri = current_url) 
+            st.session_state.flow = flow
             flow.fetch_token(code=code)
             creds = flow.credentials
             
@@ -186,9 +187,8 @@ if not URL :
     if st.button("🚀 تسجيل الدخول وربط القناة الآن", use_container_width=True):
         try:
             # flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
-            # creds = flow.run_local_server(port=0)             
-            flow = Flow.from_client_config(json.loads(st.secrets["G_CRED"]), scopes,redirect_uri = current_url)   
-            st.session_state.flow = flow
+            # creds = flow.run_local_server(port=0)  
+            flow = st.session_state.flow
             auth_url, _ = flow.authorization_url(prompt='consent')
             st.markdown(f"### [اضغط هنا لتسجيل الدخول لقناتك]({auth_url})")
             st.stop()
